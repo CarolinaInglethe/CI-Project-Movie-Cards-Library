58a358a9078cc1b53d9ae20791022b399621afee
Carol_Inglethe <carolinainglethe@gmail.com>
2021-09-21 14:58:16 -0300

    Updates

---
+++ precache-manifest.6436cfdb509f8f64f8a78b83da372627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433a2345c3a11fb7866e55497929848a",
+    "url": "/ci-project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "b52eb1747568b71fd7ff",
+    "url": "/ci-project-movie-cards-library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/ci-project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "b52eb1747568b71fd7ff",
+    "url": "/ci-project-movie-cards-library/static/js/main.8ab06a64.chunk.js"
+  },
+  {
+    "revision": "240bb04a58d31bc18182",
+    "url": "/ci-project-movie-cards-library/static/js/runtime-main.4a066beb.js"
+  }
+]);
